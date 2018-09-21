e08d2d821f58594dc9efd1be63b54917dfb9aa12
Yadullah Duman <yadullah.duman@gmail.com>
2018-09-21 17:49:33 +0200

    polished bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faGithub,
+  faTwitter,
+  faFacebookF,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons'
 
 // Import typefaces
 import 'typeface-montserrat'
@@ -6,6 +13,36 @@ import 'typeface-merriweather'
 
 import profilePic from './profile-pic.png'
 import { rhythm } from '../utils/typography'
+
+const facebookURL = 'https://www.facebook.com/yadullah.duman'
+const instagramURL = 'https://www.instagram.com/yd_76/'
+const twitterURL = 'https://twitter.com/proxyduman'
+const githubURL = 'https://github.com/yduman'
+
+const Socials = () => (
+  <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+    <div>
+      <a href={facebookURL} target="_blank">
+        <FontAwesomeIcon icon={faFacebookF} size="lg" /> Facebook
+      </a>
+    </div>
+    <div>
+      <a href={instagramURL} target="_blank">
+        <FontAwesomeIcon icon={faInstagram} size="lg" /> Instagram
+      </a>
+    </div>
+    <div>
+      <a href={twitterURL} target="_blank">
+        <FontAwesomeIcon icon={faTwitter} size="lg" /> Twitter
+      </a>
+    </div>
+    <div>
+      <a href={githubURL} target="_blank">
+        <FontAwesomeIcon icon={faGithub} size="lg" /> GitHub
+      </a>
+    </div>
+  </div>
+);
 
 class Bio extends React.Component {
   render() {
@@ -27,12 +64,9 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          Written by <strong>Yadullah Duman</strong>. I love coffee, code and
-          games. Currently a computer science student at TU Darmstadt and
-          modernizing legacy software at MaibornWolff.{' '}
-          <a href="https://twitter.com/proxyduman">
-            Follow me on Twitter
-          </a>
+          Written by <strong>Yadullah Duman</strong>. JavaScript and clean code
+          enthusiast.
+          <Socials />
         </p>
       </div>
     )
