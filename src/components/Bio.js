752c5e93675a3f31cd3120e17acb53d35481153e
Yadullah Duman <yadullah.duman@gmail.com>
2018-09-21 17:59:22 +0200

    removed changes of previous commit, because they didn't look good on mobile

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,11 +1,4 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faGithub,
-  faTwitter,
-  faFacebookF,
-  faInstagram,
-} from '@fortawesome/free-brands-svg-icons'
 
 // Import typefaces
 import 'typeface-montserrat'
@@ -13,36 +6,6 @@ import 'typeface-merriweather'
 
 import profilePic from './profile-pic.png'
 import { rhythm } from '../utils/typography'
-
-const facebookURL = 'https://www.facebook.com/yadullah.duman'
-const instagramURL = 'https://www.instagram.com/yd_76/'
-const twitterURL = 'https://twitter.com/proxyduman'
-const githubURL = 'https://github.com/yduman'
-
-const Socials = () => (
-  <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-    <div>
-      <a href={facebookURL} target="_blank">
-        <FontAwesomeIcon icon={faFacebookF} size="lg" /> Facebook
-      </a>
-    </div>
-    <div>
-      <a href={instagramURL} target="_blank">
-        <FontAwesomeIcon icon={faInstagram} size="lg" /> Instagram
-      </a>
-    </div>
-    <div>
-      <a href={twitterURL} target="_blank">
-        <FontAwesomeIcon icon={faTwitter} size="lg" /> Twitter
-      </a>
-    </div>
-    <div>
-      <a href={githubURL} target="_blank">
-        <FontAwesomeIcon icon={faGithub} size="lg" /> GitHub
-      </a>
-    </div>
-  </div>
-);
 
 class Bio extends React.Component {
   render() {
@@ -63,11 +26,10 @@ class Bio extends React.Component {
             height: rhythm(2),
           }}
         />
-        <p>
+        <div>
           Written by <strong>Yadullah Duman</strong>. JavaScript and clean code
           enthusiast.
-          <Socials />
-        </p>
+        </div>
       </div>
     )
   }
